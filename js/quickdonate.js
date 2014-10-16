e49ddec69586fd07de5f1383857b59e976a7eb97
Nileema <nileema@nileema.(none)>
2014-10-16 10:26:08 +0530

    --Extension for donation form with angular JS

---
+++ js/quickdonate.js
@@ -0,0 +1,14 @@
+(function(angular, $, _) {
+
+  var resourceUrl = CRM.resourceUrls['com.webaccessglobal.quickdonate'];
+  var quickDonation = angular.module('quickdonate', ['ngRoute']);
+
+  quickDonation.config(['$routeProvider',
+    function($routeProvider) {
+      $routeProvider.when('/donation', {
+        templateUrl: resourceUrl + '/partials/quickdonate.html',
+        controller: 'QuickDonationCtrl'
+      });
+    }
+  ]);
+})(angular, CRM.$, CRM._);
