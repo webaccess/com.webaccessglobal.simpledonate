0dadf29aa712ce11ca8008ace0d9b284296acdbf
Neha Gajbhiye <neha.gajbhiye@webaccessglobal.com>
2014-10-20 16:44:13 +0530

    Code for Zip

--- js/quickdonate.js
+++ js/quickdonate.js
@@ -12,9 +12,33 @@
     }
   ]);
 
-    quickDonation.controller('QuickDonationCtrl', function($scope) {
-      //manually binds Parsley--Validation Library to this form.
-      $('#quickDonationForm').parsley();
-      $scope.formInfo = {}; //property is set to bind input value
-    });
+   quickDonation.controller('QuickDonationCtrl', function($scope) {
+     //manually binds Parsley--Validation Library to this form.
+     $('#quickDonationForm').parsley();
+     $scope.formInfo = {}; //property is set to bind input value
+   });
+
+   quickDonation.directive('zipCodeInfo', function() {
+     var directive = {
+       require: 'ngModel',
+       link: function($scope, elm, attrs, ctrl){
+         var duration = 500;
+         var elements = {
+           country: $('#country'),
+           state: $('#state'),
+           city: $('#city')
+         }
+         elements.state.parent().hide();
+         elements.city.parent().hide();
+         elm.ziptastic().on('zipChange', function(evt, country, state, state_short, city, zip) {
+           // State
+           elements.state.val(state).parent().show(duration);
+           // City
+           elements.city.val(city).parent().show(duration);
+         });
+       },
+     };
+     return directive;
+   });
+    
 })(angular, CRM.$, CRM._);
