1b6aa60f316550e7db756da841d7dd828d724c00
Nileema <nileema@nileema.(none)>
2014-10-22 22:14:46 +0530

    --Donation form CSS and code modification

--- js/quickdonate.js
+++ js/quickdonate.js
@@ -29,46 +29,28 @@
         });
 	return deferred.promise;
       },
-      getpriceList:function() {
-        var holdPriceList = $q.defer();
-        var priceList= [];
-        var contributionId;
-
-        //get priceSetId and Amount List
-        CRM.api3('PriceField', 'get', {
-          "sequential": 1,
-          "price_set_id": CRM.quickdonate.priceSetID,
-        }).done(function(fieldVal) {
-          $.each(fieldVal.values, function(fieldKey, fieldVal) {
-            CRM.api3('PriceFieldValue', 'get', {
-              "sequential": 1,
-              "price_field_id": fieldVal.id,
-            }).done(function(amtList) {
-              $.each(amtList.values, function(amtKey, amtVal) {
-                priceList.push(amtVal);
-              });
-              holdPriceList.resolve(priceList);
-            });
-          });
-        });
-        return holdPriceList.promise;
-      },
     };
   });
 
   quickDonation.controller('QuickDonationCtrl', function($scope, formFactory) {
     //set donaiton page ID
     $scope.donationID = CRM.quickdonate.donatePageID;
+    $scope.currencySymbol = CRM.quickdonate.currency;
+    $scope.paymentProcessor = CRM.quickdonate.paymentProcessor;
+    $scope.donationConfig = CRM.quickdonate.config;
+    $scope.priceListInfo = CRM.quickdonate.priceList;
+    $scope.otherAmount = CRM.quickdonate.otherAmount;
 
+    $scope.section = 1;
     //manually binds Parsley--Validation Library to this form.
     $('#quickDonationForm').parsley();
     $scope.formInfo = {}; //property is set to bind input value
 
-    formFactory.getpriceList().then(function(data) {
-      $scope.priceListInfo = data;
-    });
+    $scope.amountActive = function(price) {
+      return $scope.amount === price;
+    }
 
-    $scope.hidePriceVal=true;
+    $scope.hidePriceVal = true;
     $scope.amountSelected = function(price) {
       $scope.amount = price;
     }
@@ -78,21 +60,79 @@
       $scope.hidePriceVal = false;
       return $scope.message;
     }
-      $scope.showInput = false;
+
+    $scope.amountInput = function() {
+	  $scope.message = $scope.formInfo.otherAmount;
+	  $scope.hidePriceVal = false;
+	  return $scope.message;
+	}
+
+	$scope.amountActive = function(price) {
+     return $scope.amount === price;
+    }
 
     $scope.selectedCardType = function(row) {
       $scope.selectedRow = row;
     };
 
-    $scope.section = 1;
     $scope.sectionShow = function() {
       $scope.section = $scope.section + 1;
     };
-    $scope.selectedSection = function(sectionS) {
-      return sectionS <= $scope.section ;
+
+    $scope.selectedSection = function(sectionNo) {
+      return sectionNo <= $scope.section ;
     };
 
+    $scope.createContribution = function (contactId,params) {
+      //get contribution page
+      var resultParams =$scope.donationConfig;//<?php echo json_encode($contributionPage);?>;
+      $scope.amount = $scope.formInfo.otherAmount || $scope.formInfo.donateAmount || 1;
+      $scope.contributionparams = {
+        "credit_card_number":$scope.formInfo.cardNumber,
+        "cvv2":$scope.formInfo.securityCode,
+        "credit_card_type":"Visa",
+        "billing_first_name":params.first_name,
+        "first_name":params.first_name,
+        "billing_middle_name":params.middle_name,
+        "middle_name":params.middle_name,
+        "billing_last_name":params.last_name,
+        "last_name":params.last_name,
+        "billing_street_address-5":params.street_address,
+        "street_address":params.street_address,
+        "billing_city-5":params.city,
+        "city":params.city,
+        "billing_country_id-5":params.country_id,
+        "country_id":params.country_id,
+        "billing_state_province_id-5":params.state_province_id,
+        "state_province_id":params.state_province_id,
+        "billing_postal_code-5":params.postal_code,
+        "postal_code":params.postal_code,
+        //"year":$scope.year,
+        //"month":$scope.month,
+        "email":params.email,
+        "contribution_page_id":resultParams.id,
+        "payment_processor_id":$scope.formInfo.payment_processor,
+        "is_test":1,
+        "total_amount":$scope.amount,
+        "financial_type_id":resultParams.financial_type_id,
+        "currencyID":resultParams.currency,
+        "currency":resultParams.currency,
+        "skipLineItem":0,
+        "skipRecentView":1,
+        "contact_id":contactId,
+        "address_id":params.address_id,
+        "source":"Online Contribution: Help Support CiviCRM!",
+      };
 
+      CRM.api3('Contribution', 'transact', $scope.contributionparams ).success(function(data, status, headers, config) {
+          if (data.is_error == 0) {
+	      alert('ok');
+          }
+          else {
+            cj('.error').html(data.error_message);
+          }
+        });
+    }
     $scope.existUserContri = function(contactId,addressID) {
       CRM.api3('Address', 'create', {
         'contact_id': "null",
@@ -105,9 +145,9 @@
         'is_billing': 1,
         'is_primary': 0,
         'api.Address.update':{'id':addressID, 'contact_id': $scope.contact_id, 'is_billing':1,'street_address':$scope.formInfo.address,'city':$scope.formInfo.city,'postal_code':$scope.formInfo.zip},
-        "api.Contribution.create":{ "financial_type_id": 1,"total_amount": $scope.amount,"contact_id": contactId,"payment_instrument_id": 1,"address_id":'$value.id','net_amount':$scope.amount }
-      }).success(function(data, status, headers, config) {
-        alert('DONE11111111!!');
+      });
+      formFactory.getUser(contactId).then(function(resultParams) {
+        $scope.createContribution(contactId,resultParams);
       });
     }
     $scope.newUserContri = function(contactId) {
@@ -124,13 +164,11 @@
            'is_billing':1,
            'is_primary':primaryValue[key]
         });
-     });
-     formFactory.getUser(contactId).then(function(resultParams) {
-       CRM.api3('Contribution', 'create', {"financial_type_id": 1,"total_amount": 100,"contact_id": contactId,"payment_instrument_id": 1,"address_id":resultParams.address_id,'net_amount': 100}).success(function(data, status, headers, config) {
-          alert('DONE22222222!!');
-        });
       });
-    }
+      formFactory.getUser(contactId).then(function(resultParams) {
+        $scope.createContribution(contactId,resultParams);
+      });
+    };
 
     $scope.saveData = function() {
       $scope.amount = $scope.formInfo.otherAmount || $scope.formInfo.donateAmount;
@@ -138,7 +176,7 @@
           "email": $scope.formInfo.email,
         "contact_type":"Individual"
       }).success(function(data, status, headers, config) {
-          var counter = 0, resultParams = {};
+        var counter = 0, resultParams = {};
         cj.each( data.values, function( key, value ) {
           counter++;
           if (counter == 1) {
@@ -160,9 +198,36 @@
              $scope.newUserContri(data.id);
           });
         }
-
         $scope.quickDonationForm.$setPristine();
       });
+    };
+    $scope.creditType = false;
+    $scope.directDebitType = false;
+    $scope.hiddenProcessor = false;
+
+    $scope.setPaymentBlock = function(value) {
+	$billingmodeform = 1;//billing-mode-form = 1
+	//billing-mode-button = 2
+	//billing-mode-notify = 4
+	//payment-type-credit-card = 1
+	//payemt-type-direct-debit = 2
+	if($scope.paymentProcessor[value]['billing_mode'] & $billingmodeform /*billing_mode_form*/) {
+	  if($scope.paymentProcessor[value]['payment_type'] == 1) {
+	    $scope.creditType = true;
+	    $scope.directDebitType = false;
+	    $scope.hiddenProcessor = false;
+	  }
+	  else if($scope.paymentProcessor[value]['payment_type'] == 2) {
+	    $scope.directDebitType = true;
+	    $scope.creditType = false;
+	    $scope.hiddenProcessor = false;
+	  }
+	}
+	else {
+	  $scope.hiddenProcessor = true;
+	  $scope.creditType = false;
+	  $scope.directDebitType = false;
+	}
     }
   });
 
@@ -170,7 +235,26 @@
     var directive = {
       require: 'ngModel',
       link: function(scope, elm, attrs, ctrl){
-        $(elm).inputmask({mask: "m/q"});
+        $(elm).inputmask({mask: "m/q", placeholder: " "});
+        elm.bind('keyup', function() {
+          if (scope.quickDonationForm.cardExpiry.$valid && elm.val().length==5) {
+            $('#securityCode').focus();
+	  }
+        });
+      }
+    }
+    return directive
+  });
+
+  quickDonation.directive('securityCode', function(){
+    var directive = {
+      require: 'ngModel',
+      link: function(scope, elm, attrs, ctrl){
+        elm.bind('keyup', function(){
+          if (scope.quickDonationForm.securityCode.$valid) {
+            $('#zip').focus();
+	  }
+        });
       }
     }
     return directive
@@ -180,13 +264,30 @@
     var directive = {
       require: 'ngModel',
       link: function(scope, elm, attrs, ctrl){
-        $(elm).inputmask({ mask: "9999 9999 9999 9999"});
+        scope.cardNumberValue = null;
+        $(elm).inputmask({ mask: "9999 9999 9999 9999", placeholder: " "});
+
+        elm.bind('keyup', function(){
+	  if (scope.quickDonationForm.cardNumber.$valid) {
+            if (scope.type) {
+              $('#card-expiration').focus();
+            }
+	  }
+        });
+        elm.bind('blur', function(){
+	  var val = elm.val();
+	  scope.cardNumberValue = val;
+	  elm.val(val.substr(val.length - 4));
+	});
+        elm.bind('click', function(){
+	    elm.val(scope.cardNumberValue);
+	});
 
         ctrl.$parsers.unshift(function(value){
           scope.type =
             (/^5[1-5]/.test(value)) ? "mastercard"
             : (/^4/.test(value)) ? "visa"
-            : (/^3[47]/.test(value)) ? 'amex'
+            : (/^3[4|7]/.test(value)) ? 'amex'
             : (/^6011|65|64[4-9]|622(1(2[6-9]|[3-9]\d)|[2-8]\d{2}|9([01]\d|2[0-5]))/.test(value)) ? 'discover'
             : undefined
             ctrl.$setValidity('invalid',!!scope.type);
